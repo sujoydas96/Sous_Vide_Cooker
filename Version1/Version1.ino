6c2ed7384e672e8987330a3147ecc19c97dd7fc2
Sujoy Das <das.4567@gmail.com>
2023-08-11 11:19:34 +0530

    added the connection of temperature sensor. Pins also defined in the program

--- Version1/Version1.ino
+++ Version1/Version1.ino
@@ -1,17 +1,30 @@
-//This is a blinking Program. We have to build a program that runs a buzzer for high and low temperature detection, one DS thermometer Probes, 3 lights for temperature checking
+//This is a blinking Program. We have to build a program that runs a buzzer for high and low temperature detection, one DS18B20 thermometer Probes, 3 lights for temperature checking
 //Version 1 is for a low budget project
 //Version 2 will have a buzzer for high and low temperature detection, two thermometer probes (average temperature will be calculated), 3 lights for temperature checking and 1 LCD display to show current temperature reading.
 
-// the setup function runs once when you press reset or power the board
+
+//                                                 CONNECTIONS
+//      CONNECTING THE TEMPERATURE SENSOR
+//         1. Connect one 4.7k ohm resistor from the power(5V) line to the data line of the temperature probe. 
+//         2. Connect the Drain and Power lines of the sensor to the 5V and GND pins in the arduino respectively
+//         3. Data line for the sensor is at Digital 30 of the arduino.
+//         4. Connect the Data pin of the sensor to Digital pin 30 of the arduino mega and make sure that power passes through the line without running through the 4.7k ohm resistor.
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  //Digital Pins for Arduino Mega
+  //The Termperature sensor is working in non-parasite mode.
+  
+  pinMode(22, OUTPUT);     //Pin for Buzzer
+  pinMode(24, OUTPUT);     //Pin for Blue  LED
+  pinMode(25, OUTPUT);     //Pin for Green LED
+  pinMode(26, OUTPUT);     //Pin for Red   LED
+  pinMode(30, INPUT);      //Pin for Input for DS18B20
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  //Beeping the buzzer
+  digitalWrite(22, HIGH);  
+  delay(1000);                      
+  digitalWrite(22, LOW);  
+  delay(1000);                      
 }
